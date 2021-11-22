8b9194475b2eeeac94d3760c3046ee9f5459e9ca
up201906355 <up201906355@fe.up.pt>
2021-11-22 20:26:23 +0000

    [Documentation] ll functions Doxygen documentation

--- proj1/dataLayer.h
+++ proj1/dataLayer.h
@@ -1,40 +1,51 @@
 #ifndef DATALAYER_H
 #define DATALAYER_H
 
-#include "macros.h"
+#include "sender.h"
+#include "receiver.h"
+
+//int id; /* Receiver -> 1 | Sender -> 0 */
 
 /**
- * @brief Open file
+ * @brief Open serial port connection through a file descriptor, in a specific
+ * side of the serial port, given by the id
  *
  * @param  filename               file path to be transmmited
- * @param  identity               Receiver -> 1 | Sender -> 0
+ * @param  id                     Receiver -> 1 | Sender -> 0
  * @return          file descriptor upon sucess, ERROR otherwise
  */
-int llopen(char *filename, int identity);
+int llopen(char *filename, int id);
 
 /**
- * @brief Closes file connection
+ * @brief Closes the serial port connection, in a specific
+ * side of the serial port, given by the id
+ *
  * @param  fd               File descriptor of filename(llopen) file
- * @return
+ * @param  id               Receiver -> 1 | Sender -> 0
+ * @return          SUCCESS upon success, ERROR otherwise
  */
-int llclose(int fd);
+int llclose(int fd, int id);
 
 /**
  * @brief Sends a packet with the data , the data can't extend the specified max length
+ *
  * @param  fd                   File descriptor of the RS-232 port
  * @param  buffer               Data to be written to serial port
  * @param  size                 Buffer size
+ * @param  id                   Receiver -> 1 | Sender -> 0
  * @return        Data size upon sucess, ERROR otherwise
  */
-int llwrite(int fd, char *buffer, int size);
+int llwrite(int fd, char *buffer, int size, int id);
 
 /**
  * @brief Receives packet with the data, sent throw the serial port
+ *
  * @param  fd                   File descriptor of the RS-232 serial port
  * @param  buffer               Buffer with data read from the serial port
+ * @param  id                   Receiver -> 1 | Sender -> 0
  * @return        Data size upon sucess, ERROR otherwise
  */
-int llread(int fd, char *buffer);
+int llread(int fd, char *buffer, int id);
 
 
 
